b860f22e617de4e565fa3ec4bc941cb15a416c88
HP <HP@DESKTOP-CQINKFU>
2023-07-27 12:23:20 +0530

    FIRST COMMIT

---
+++ C2TC/src/com/cg/dayone/Basics.java
@@ -0,0 +1,21 @@
+package com.cg.dayone;
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int a=20;
+ int b=10;
+ int sum=0,sub=0,mul=0,div=0;
+ System.out.println("a= " + a);
+ System.out.println("b= " + b);
+ sum = a+b;
+ sub = a-b;
+ mul = a*b;
+ div = a/b;
+ System.out.println("sum= " + sum);
+ System.out.println("sub= " + sub);
+ System.out.println("mul= " + mul);
+ System.out.println("div= " + div);
+	}
+}
